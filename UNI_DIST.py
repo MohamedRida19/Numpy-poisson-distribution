94fdb25153b66782b73adb8ff9cf87a0e0da6fd3
Medrida19 <mohamedajhir098@gmail.com>
2023-12-06 11:32:34 +0000

    showing the figure program

--- UNI_DIST.py
+++ UNI_DIST.py
@@ -3,4 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 """
 x = np.random.uniform(size=10).reshape(2,5)
-print(x)"""
+print(x)
+"""
+
+#showing the figure
+a= 0
+b= 10
+Uniform_data = np.random.uniform( a, b,size=100)
+Uniform_data = np.clip(Uniform_data, a,b)
+sns.displot(Uniform_data, kde = True, linewidth= 0, fill= 0, color = 'red', label= 'UNIFORM')
+
+plt.legend()
+plt.show()
