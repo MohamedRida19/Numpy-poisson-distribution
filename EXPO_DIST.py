e17d0ca5bf4778c6e121853f078ece1a1efd9f82
Medrida19 <mohamedajhir098@gmail.com>
2023-12-06 12:15:48 +0000

    Showing the figure program

--- EXPO_DIST.py
+++ EXPO_DIST.py
@@ -4,3 +4,8 @@ import seaborn as sns
 
 '''x = np.random.exponential(scale = 2.5, size= (2,4))
 print(x)'''
+
+expo_data = np.random.exponential(scale = 7, size =1000)
+sns.histplot(expo_data, kde=False, color='red', label= 'EXPO DIST', fill=0)
+plt.legend()
+plt.show()
