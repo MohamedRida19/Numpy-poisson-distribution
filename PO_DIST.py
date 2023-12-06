4f8ae209240170ef9f4a0c949cf4f7cf06bfd5eb
Medrida19 <mohamedajhir098@gmail.com>
2023-12-06 10:18:14 +0000

    storing the code

--- PO_DIST.py
+++ PO_DIST.py
@@ -2,8 +2,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-x = np.random.poisson(lam= 3, size= 1000)
+"""x = np.random.poisson(lam= 3, size= 1000)
 sns.histplot(x, kde = False, label = 'Graph1')
 
 plt.legend()
-plt.show()
+plt.show()"""
+
