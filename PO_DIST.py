d35f5f00884f11bae853d027b8c486552eb4b11c
Medrida19 <mohamedajhir098@gmail.com>
2023-12-06 10:41:50 +0000

    Adding comments + creating a program to compare the normal distribution and the poisson distribution

--- PO_DIST.py
+++ PO_DIST.py
@@ -1,13 +1,14 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+#test
 """x = np.random.poisson(lam= 3, size= 1000)
 sns.histplot(x, kde = False, label = 'Graph1')
 
 plt.legend()
 plt.show()"""
 
+#normal data vs poisson data
 """normal_data = np.random.normal(loc = 50, scale = 7, size =1000)
 poisson_data = np.random.poisson(lam = 50, size = 1000)
 
@@ -16,3 +17,11 @@ sns.displot(poisson_data, kde= True, linewidth = 0, color= "green", fill = 0)
 
 plt.show()"""
 
+#binomial data vs poisson data
+binomial_data = np.random.binomial(n = 50, p= 0.4, size = 1000)
+poisson_data = np.random.poisson(lam = 50, size = 1000)
+
+sns.displot(binomial_data, kde = True, linewidth = 0, fill=0, color = "red")
+sns.displot(poisson_data, kde = True, linewidth= 0, fill = 0, color = 'green')
+
+plt.show()
