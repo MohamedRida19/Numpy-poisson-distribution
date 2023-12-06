961e2db8bca620d267431e83779cd6c41542a106
Medrida19 <mohamedajhir098@gmail.com>
2023-12-06 10:35:29 +0000

    Storing the comparison

--- PO_DIST.py
+++ PO_DIST.py
@@ -8,10 +8,11 @@ sns.histplot(x, kde = False, label = 'Graph1')
 plt.legend()
 plt.show()"""
 
-normal_data = np.random.normal(loc = 50, scale = 7, size =1000)
+"""normal_data = np.random.normal(loc = 50, scale = 7, size =1000)
 poisson_data = np.random.poisson(lam = 50, size = 1000)
 
 sns.displot(normal_data, kde= True, linewidth = 0, color= "red", fill = 0)
 sns.displot(poisson_data, kde= True, linewidth = 0, color= "green", fill = 0)
 
-plt.show()
+plt.show()"""
+
